332378bd592f932be948c7343c23e328185f2d6d
ethantenison <tenison.ethan@gmail.com>
2022-01-19 12:33:48 -0600

    Before switching to golem

--- app.R
+++ app.R
@@ -72,7 +72,120 @@ ui <- dashboardPage(
         '><i class='fa fa-expand fa-pull-left'></i> Fullscreen</button>"
             )
     )),
-    dashboardBody()
+    ## Body ----
+    dashboardBody(
+        
+        tags$head(
+            tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
+        ),
+        tags$script(
+            HTML(
+                '// Sets the name next to navbar
+      $(document).ready(function() {
+        $("header").find("nav").append(\'<span class="myClass", style="color:white"><b>Texas Water Foundation </b></span>\');
+      }),
+      $(document).ready(function(){
+  $("a[data-toggle=tab]").on("show.bs.tab", function(e){
+    Shiny.setInputValue("activeTab", $(this).attr("data-value"));
+  });
+});
+     '
+            )
+        ),
+
+tabItems(
+    ### Welcome ----
+    tabItem(tabName = "welcome",
+            column(
+                width = 12,
+                fluidRow(
+                    shinydashboard::box(
+                        title = NULL,
+                        width = 12,
+                        solidHeader = TRUE,
+                        background = "blue",
+                        status = "primary",
+                        br(),
+                        fluidRow(
+                            column(
+                                width = 9,
+                                h1(strong("Welcome to the TWF Dashboard!"),
+                                   style = "font-size:5em;")),
+                            column(
+                                width = 3,
+                                div(
+                                    style = "text-align: center;padding-right:30px;
+                      padding-bottom: 15px;padding-top: 30px;", 
+                      appButton(
+                          inputId = "welcome_bt",
+                          label = "Welcome Guide",
+                          enable_badge = TRUE, 
+                          icon = icon("book-open"),
+                          badgeColor = "red", 
+                          badgeLabel = 3,
+                          width = '100%'
+                      ))
+                            ))
+                    )
+                ),
+                fluidRow(
+                    column(12,
+                           style = "padding-left:30px;",
+                           h1("Brought to you by:"))
+                )
+            )),
+    ### AQ ----
+    tabItem(tabName = "geo",
+            column(
+                width = 12,
+                offset = 0,
+                fluidRow(
+                    column(style = 'padding-left:30px;padding-bottom: 15px;padding-top: 0px;',
+                           width = 9,
+                           h1(strong("Geography")))),
+                fluidRow(column(
+                    width = 12,
+                    shinydashboard::box(
+                        width = 8,
+                        solidHeader = FALSE,
+                        status = "primary",
+                        # dataUI(
+                        #     #Selector UI
+                        #     "air",
+                        #     choices = list(
+                        #         "O3",
+                        #         "Ozone - CAPCOG",
+                        #         "PM2.5",
+                        #         "PM2.5 - CAPCOG",
+                        #         "Percentile for PM2.5"
+                        #     ),
+                        #     selected = "PM2.5"
+                        # ),
+                        # mapUI("air_map", height = "650") #Map UI
+                    ),
+                    shinydashboard::box(
+                        width = 4,
+                        solidHeader = FALSE,
+                        status = "primary",
+                        #plotsUI("aq_bar")
+                    )
+                    
+                ))
+            )),
+    ### Attributions ----
+    tabItem(
+        tabName = "attributions",
+        column(
+            width = 10,
+            offset = 1,
+            style = 'padding-left:30px;padding-bottom: 15px;padding-top: 0px;',
+            h1(strong("Attributions"))
+            
+
+        )
+    )
+    )
+)
 )
 
 # Server ----
